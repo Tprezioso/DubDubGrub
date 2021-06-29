a267c16412f1e3331df25ca520c1b67895b20195
Thomas <tommyprezioso@gmail.com>
2021-06-29 12:42:44 -0400

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Thomas Prezioso Jr on 6/29/21.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
