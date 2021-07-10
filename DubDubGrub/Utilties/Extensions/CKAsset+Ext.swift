10dfa64269e296cb77c762fd97803655769cbdf2
Thomas <tommyprezioso@gmail.com>
2021-07-10 12:53:30 -0400

    Location List, Detail and CKAsset to UIImage

---
+++ DubDubGrub/Utilties/Extensions/CKAsset+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  CKAsset+Ext.swift
+//  DubDubGrub
+//
+//  Created by Thomas Prezioso Jr on 7/10/21.
+//
+
+import CloudKit
+import UIKit
+
+extension CKAsset {
+    func convertToUIImage(in dimension: ImageDimension) -> UIImage {
+        let placeholder = ImageDimension.getPlaceholder(for: dimension)
+        
+        guard let fileURL = self.fileURL else { return placeholder }
+        
+        do {
+            let data = try Data(contentsOf: fileURL)
+            return UIImage(data: data) ?? placeholder
+        } catch {
+            return placeholder
+        }
+    }
+}
